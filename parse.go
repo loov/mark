7ef722ccfbf32df931832ab2b93cc83f9cc6026f
Egon Elbre <egonelbre@gmail.com>
2016-01-14 11:25:37 +0200

    Remove todo note.

--- parse.go
+++ parse.go
@@ -391,8 +391,6 @@ func (parent *parse) include() {
 	parentreader := parent.reader
 	parent.flushParagraph()
 
-	//TODO: add recursion protection
-
 	parentreader.ignoreN('{', 2)
 	parentreader.ignoreTrailing('}')
 
